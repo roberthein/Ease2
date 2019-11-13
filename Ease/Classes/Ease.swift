6d2dfb9ac9d139a030be9e32c2cf5fcf05757355
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2019-11-13 21:22:23 +0100

    🤘

--- Ease/Classes/Ease.swift
+++ Ease/Classes/Ease.swift
@@ -12,6 +12,11 @@ public final class Ease<E: Easable> {
         target = value
     }
     
+    /// Add spring parameters for the spring animation
+    /// - Parameters:
+    ///   - tension: The tension of the spring.
+    ///   - damping: The dampig the spring is subjected to.
+    ///   - mass: The mass of the subject.
     public func spring(t tension: E.Scalar, d damping: E.Scalar, m mass: E.Scalar, rubberBandRange: EaseRange<E>? = nil, rubberBandResilience: E.Scalar? = nil, clampRange: EaseRange<E>? = nil) -> AnyPublisher<E, Never> {
         let key = keys.next()!
         publishers[key] = EasePublisher(target, spring: EaseSpring(tension, damping, mass))
